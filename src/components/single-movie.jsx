2d6d88ae3cfcf1d8d93288ea47286709366446fd
umair1191 <umair.1191@gmail.com>
2021-08-27 16:30:02 +0500

    Update single-movie.jsx

--- src/components/single-movie.jsx
+++ src/components/single-movie.jsx
@@ -1,31 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-
-const minutesToHms = (minues) => {
-  var Inseconds = minues * 60;
-  var sec_num = parseInt(Inseconds);
-  var h = Math.floor(sec_num / 3600);
-  var m = Math.floor((sec_num - h * 3600) / 60);
-  var s = sec_num - h * 3600 - m * 60;
-
-  var hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
-  var mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
-  var sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
-
-  return hDisplay + mDisplay + sDisplay;
-};
+import { minutesToHms } from "../functions/functions";
 
 const SingleMovie = ({ movieinfo }) => {
-
   return (
     <div className="row mt-5">
       <div className="col-12 detail-page-nav">
-        <h1>Movie Deatil Page</h1> 
+        <h1>Movie Deatil Page</h1>
         <Link to={{ pathname: `/` }}>
-          <button type="button" className="btn btn-primary ">Back</button>
+          <button type="button" className="btn btn-primary ">
+            Back
+          </button>
         </Link>
-
       </div>
       <div className="col-2 mt-5">
         <img
@@ -37,11 +23,13 @@ const SingleMovie = ({ movieinfo }) => {
 
       <div className="col-10  mt-5">
         <h2>{movieinfo.title}</h2>
-        <span><i>{movieinfo.tagline}</i></span>
+        <span>
+          <i>{movieinfo.tagline}</i>
+        </span>
         <div>
           Realease date : {movieinfo.release_date}
           <br />
-          Types :{" "}
+          Types :
           {movieinfo.genres
             ? movieinfo.genres.map((item, i) => (
                 <span key={i}>{item.name} </span>
